bf42e58442570f75904871dbec983ba58736794a
Carter Jiang <yejiangholy@gmail.com>
2016-07-19 16:48:36 -0400

    Get rid of Facebook login

--- LetsChart/RegisterFBUser.swift
+++ LetsChart/RegisterFBUser.swift
@@ -10,16 +10,15 @@ import Foundation
 
 public func registerUserDeviceId() {
     
-    if backendless.messagingService.getRegistration().deviceId != nil {
+    if (backendless.messagingService.getRegistration().deviceId != nil) {
         
         let deviceId = backendless.messagingService.getRegistration().deviceId
         
         let properties = ["deviceId" : deviceId]
         
-       backendless.userService.currentUser.updateProperties(properties)
+        backendless.userService.currentUser!.updateProperties(properties)
         backendless.userService.update(backendless.userService.currentUser)
     }
-    
 }
 
 
